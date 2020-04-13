09158887c4481d37453ffad8bdebd543c6fd7601
Muhammad Shahrukh <20100287@lums.edu.pk>
2020-04-13 22:03:46 +0500

    final touches

--- pokemon.py
+++ pokemon.py
@@ -7,14 +7,26 @@ typeAdvantages={
     "Rock": []
 }
 
+evolutions={
+    "Charmander":[16, "Charmeleon"],
+    "Charmeleon":[36, "Charizard"],
+
+}
+
 class Pokemon:
     def __init__(self, name, level, pokeType, faint=False):
         self.name=name
         self.type=pokeType
         self.level=level 
+        if level>100:
+            self.level=100
         self.maxHealth=level*5
         self.currentHealth=self.maxHealth
         self.isFaint=faint
+        self.experience=0 #I will keep threshold of level*2 for leveling up
+        self.nextEvolution=-1
+        if self.name in evolutions:
+            self.nextEvolution=evolutions[self.name][0]
 
     def knockOut(self):
             self.currentHealth=0
@@ -39,11 +51,43 @@ class Pokemon:
         self.currentHealth=self.maxHealth/2
         self.isFaint=False
         print(f"{self.name} has been revived. It now has {self.currentHealth} health!")
+
+    def levelUp(self, remainingExp=0):
+        self.level+=1
+        self.experience=remainingExp
+        print(f"{self.name} leveled up! It's now at level {self.level}.")
+        if self.nextEvolution!=-1 and self.level>=self.nextEvolution:
+            self.evolve()
+    
+    def evolve(self):
+        evolutionName= evolutions[self.name][1]
+        oldName=self.name 
+        if evolutionName in evolutions:
+            self.nextEvolution= evolutions[evolutionName][0]
+        else:
+            self.nextEvolution=-1
+        self.name=evolutionName
+        print(f"{oldName} evolved into {self.name}!")
+
+    def gainXP(self, otherPokeLevel):
+        #XP gained will be 0.25* level of the other pokemon I beat
+        self.experience+= 0.25*otherPokeLevel
+        threshold=self.level*2
+        print(f"{self.name} gained {0.25*otherPokeLevel} experience from the battle!")
+        if self.experience>= threshold:
+            self.levelUp(self.experience-threshold)
+        
+    
+        
     
     
     def attack(self, enemy):
         #Dealing with Water, Fire, Grass, Ice, and Rock types
         damage=0
+        
+        if self.isFaint:
+            print(f"{self.name} has fainted, it cannot attack! Please Switch Pokemon")
+            return 
         if enemy.type in typeAdvantages[self.type]:
             #do double damage
             damage=self.level*2
@@ -100,23 +144,52 @@ class Trainer:
         
     def printCurrPoke(self):
         print(f"{self.name}'s Current Active Pokemon: {self.pokeTeam[self.currPoke]}")
+
+    def checkAllFainted(self):
+        for poke in self.pokeTeam:
+            if poke.isFaint==False:
+                return False 
+        return True 
+
+
+class Charmander(Pokemon):
+    def __init__(self, specialMove, level=5):
+        super().__init__("Charmander", level, "Fire")
+        self.specialMove= specialMove
+    
+    def attack(self, enemy):
+        print(f"{self.name} used {self.specialMove}!")
+        super().attack(enemy)
+
+
+
+
 ####Testing
-alleosPoke=[Pokemon("Charizard", 50, "Fire"), Pokemon("Swampert", 50, "Water")]
+#Pokemon("Charizard", 50, "Fire")
+alleosPoke=[Charmander("Flamethrower",15), Pokemon("Swampert", 50, "Water")]
 alleo= Trainer("Alleo", 5, alleosPoke )
 garysPoke=[Pokemon("Venasaur", 50, "Grass"), Pokemon("Onyx", 50, "Rock")]
 gary= Trainer("Gary",0, garysPoke)
 
 alleo.printCurrPoke()
+print("Alleo's Current Pokemon")
 for poke in alleo.pokeTeam:
     poke.printStats()
+
+print("Gary's current Pokemon")
 for poke in gary.pokeTeam:
     poke.printStats()
+
+
 alleo.attackOther(gary)
 gary.attackOther(alleo)
-alleo.attackOther(gary)
-gary.attackOther(alleo)
-alleo.attackOther(gary)
+alleosPoke[0].levelUp()
+alleo.printCurrPoke()
+
+alleo.switchPoke("Swampert")
+alleo.printCurrPoke()
+
+gary.printCurrPoke()
 gary.switchPoke("Onyx")
-# for x in garysPoke:
-#     if x.__repr__()=="Venasaur":
-#         print(x.level)
+gary.printCurrPoke()
+
