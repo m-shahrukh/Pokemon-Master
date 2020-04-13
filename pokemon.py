25a41cc69f1179336c0216a2998421ce2da69da2
Muhammad Shahrukh <20100287@lums.edu.pk>
2020-04-13 22:12:13 +0500

    Added comments

--- pokemon.py
+++ pokemon.py
@@ -24,7 +24,7 @@ class Pokemon:
         self.currentHealth=self.maxHealth
         self.isFaint=faint
         self.experience=0 #I will keep threshold of level*2 for leveling up
-        self.nextEvolution=-1
+        self.nextEvolution=-1 #if there's no next evo, keep this at -1 else set it to the level of the next evolution
         if self.name in evolutions:
             self.nextEvolution=evolutions[self.name][0]
 
@@ -34,6 +34,7 @@ class Pokemon:
             print(f"{self.name} fainted!")
     
     def loseHealth(self, damage):
+        #Health cannot go below 0
          if self.currentHealth-damage<=0:   
             self.knockOut()
          else:
@@ -41,6 +42,7 @@ class Pokemon:
              print(f"{self.name} now has {self.currentHealth} health!")
     
     def gainHealth(self, recovery):
+        #Health cannot go over max health
          if recovery+self.currentHealth>=self.maxHealth:
              self.currentHealth=self.maxHealth
          else:
@@ -48,11 +50,13 @@ class Pokemon:
          print(f"{self.name} now has {self.currentHealth} health!")
     
     def revive(self):
+        #In games, the revived pokemon usually gets half its health restored
         self.currentHealth=self.maxHealth/2
         self.isFaint=False
         print(f"{self.name} has been revived. It now has {self.currentHealth} health!")
 
     def levelUp(self, remainingExp=0):
+        #level up, if I reach an evolution, evolve my poke
         self.level+=1
         self.experience=remainingExp
         print(f"{self.name} leveled up! It's now at level {self.level}.")
@@ -60,6 +64,9 @@ class Pokemon:
             self.evolve()
     
     def evolve(self):
+        #Get the evolved pokemon name, change name to that and check if there's any further evolution
+        #then store that level next e.g charmander evolves into Charmeleon
+        #now Charmeleon's next evolution var will have 36 as the level for Charizard evo
         evolutionName= evolutions[self.name][1]
         oldName=self.name 
         if evolutionName in evolutions:
@@ -84,7 +91,8 @@ class Pokemon:
     def attack(self, enemy):
         #Dealing with Water, Fire, Grass, Ice, and Rock types
         damage=0
-        
+        #Attack according to type advantage, the dictionary defining them is declared at the top
+
         if self.isFaint:
             print(f"{self.name} has fainted, it cannot attack! Please Switch Pokemon")
             return 
@@ -118,16 +126,20 @@ class Trainer:
         
     def usePotion(self):
         if self.numPotions>0:
-            self.pokeTeam[self.currPoke].gainHealth(20)
             print(f"{self.name} used a Potion!")
+            self.pokeTeam[self.currPoke].gainHealth(20)
+            
         else:
             print(f"{self.name} doesn't have any potions left!")
     
     def attackOther(self, otherTrainer):
+        #get pokemon using other's currPoke index from their team and attack that poke
         self.pokeTeam[self.currPoke].attack(otherTrainer.pokeTeam[otherTrainer.currPoke])
 
     def switchPoke(self, pokeToSwitch):
-        #pokeToSwitch is a name
+        #pokeToSwitch is a name 
+        #Get corresponding pokemon from team, if it doesn't exist, raise the error
+        #otherwise switch pokemon
         pokemon=None
         for x in self.pokeTeam:
             if x.__repr__()==pokeToSwitch:
@@ -145,7 +157,7 @@ class Trainer:
     def printCurrPoke(self):
         print(f"{self.name}'s Current Active Pokemon: {self.pokeTeam[self.currPoke]}")
 
-    def checkAllFainted(self):
+    def checkAllFainted(self): #For future use: if i implement a battle
         for poke in self.pokeTeam:
             if poke.isFaint==False:
                 return False 
@@ -157,7 +169,7 @@ class Charmander(Pokemon):
         super().__init__("Charmander", level, "Fire")
         self.specialMove= specialMove
     
-    def attack(self, enemy):
+    def attack(self, enemy): #First display its special move, then call the original parent function
         print(f"{self.name} used {self.specialMove}!")
         super().attack(enemy)
 
@@ -166,10 +178,12 @@ class Charmander(Pokemon):
 
 ####Testing
 #Pokemon("Charizard", 50, "Fire")
+
+#Initialize two trainers with two pokes each
 alleosPoke=[Charmander("Flamethrower",15), Pokemon("Swampert", 50, "Water")]
-alleo= Trainer("Alleo", 5, alleosPoke )
+alleo= Trainer("Alleo", 5, alleosPoke ) #5 potions
 garysPoke=[Pokemon("Venasaur", 50, "Grass"), Pokemon("Onyx", 50, "Rock")]
-gary= Trainer("Gary",0, garysPoke)
+gary= Trainer("Gary",0, garysPoke) #0 potions
 
 alleo.printCurrPoke()
 print("Alleo's Current Pokemon")
@@ -180,16 +194,21 @@ print("Gary's current Pokemon")
 for poke in gary.pokeTeam:
     poke.printStats()
 
-
+#Alleo attacks gary who then attacks back, alleo uses a potion
 alleo.attackOther(gary)
+gary.usePotion() #He has 0 so he wont be able to!
 gary.attackOther(alleo)
+alleo.usePotion()
+#we level up charmander and see its evolution
 alleosPoke[0].levelUp()
-alleo.printCurrPoke()
+alleo.printCurrPoke() #print current pokemon to see updated evolved pokemon
 
-alleo.switchPoke("Swampert")
-alleo.printCurrPoke()
+alleo.switchPoke("Swampert") 
+alleo.printCurrPoke() #print current pokemon after switching 
 
-gary.printCurrPoke()
+#same for gary
+gary.printCurrPoke() 
 gary.switchPoke("Onyx")
 gary.printCurrPoke()
+
 
